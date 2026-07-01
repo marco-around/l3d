94b05bf0e19247035925a452c32969e000a6c1bd
marco-trds <marcosdevprofessional@gmail.com>
2026-07-01 11:04:22 -0300

    chore: add tailwind css in web project

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,3 +1,7 @@
 export function App() {
-	return <div>Hello</div>
+	return (
+		<div className="min-h-screen flex text-2xl font-bold bg-zinc-800 text-zinc-200 items-center justify-center">
+			Hello
+		</div>
+	)
 }
