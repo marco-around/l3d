95d7ce8da759ab60b38d5f797d9ae4ff07f0ad53
marco-trds <marcosdevprofessional@gmail.com>
2026-07-02 23:49:54 -0300

    feat: add api reference with scalar and swagger

---
+++ apps/api/src/config/swagger.ts
@@ -0,0 +1,28 @@
+import type { INestApplication } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
+import { apiReference } from '@scalar/nestjs-api-reference'
+
+export function setupDocumentation(app: INestApplication) {
+	const config = new DocumentBuilder()
+		.setTitle('L3D API Reference')
+		.setDescription('API reference for the L3D project')
+		.setVersion('0.0.0')
+		.build()
+
+	const documentFactory = () => SwaggerModule.createDocument(app, config)
+
+	SwaggerModule.setup('api', app, documentFactory, {
+		ui: false,
+		jsonDocumentUrl: 'openapi.json',
+	})
+
+	app.use(
+		'/reference',
+		apiReference({
+			url: '/openapi.json',
+			withFastify: true,
+			theme: 'laserwave',
+		})
+	)
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { FastifyAdapter, type NestFastifyApplication } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
+import { setupDocumentation } from './config/swagger'
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+	setupDocumentation(app)
 	await app.listen(process.env.PORT ?? 3000, '0.0.0.0')
 }
 bootstrap()
